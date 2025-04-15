0ed9db0c4205f13494fad9be3ff1f1a0468cbbd8
Frose3 <ruzickafilip1@gmail.com>
2025-04-15 18:25:59 +0200

    changed request to httpx in fullhunt.py

--- osint_tools/fullhunt.py
+++ osint_tools/fullhunt.py
@@ -2,6 +2,7 @@ import requests
 import base64
 import configparser
 from datetime import date
+import httpx
 
 def fullhunt(user_query):
     config = configparser.ConfigParser()
@@ -15,9 +16,12 @@ def fullhunt(user_query):
 
     headers = {
         'X-API-KEY': fullhunt_api_key,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Connection': 'close'
     }
 
-    response = requests.get(f"https://fullhunt.io/api/v1/domain/{user_query}/details", headers=headers)
+    url = f"https://fullhunt.io/api/v1/domain/{user_query}/details"
+    response = httpx.get(url, headers=headers)
 
     if response.json().get('hosts'):
         return response.json()['hosts']
